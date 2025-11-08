bcda927134d3480085777549a8dc45b48dd00b3c
Michael Holtzscher <michael@holtzscher.com>
2025-11-08 16:40:02 -0600

    add wallpaper

--- modules/home-manager/hosts/desktop/niri.nix
+++ modules/home-manager/hosts/desktop/niri.nix
@@ -1,8 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # Niri scrollable-tiling Wayland compositor config
   # This is a minimal experimental setup - feel free to customize!
 
   # Create Niri configuration file from external KDL file
   home.file.".config/niri/config.kdl".text = builtins.readFile ../../files/niri/config.kdl;
+
+  # Copy wallpaper to a known location
+  home.file.".config/wallpapers/current.jpg".source = ../../files/wallpapers/aishot-3308.jpg;
+
+  # Install swaybg for wallpaper
+  home.packages = with pkgs; [ swaybg ];
 }
