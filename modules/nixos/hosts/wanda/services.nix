51241a9ca36476444523fc4b75a23b10cd8fa655
Michael Holtzscher <michael@holtzscher.com>
2025-12-08 22:13:06 -0600

    refactor: nest services under services attribute in wanda config
    Improves code organization by consolidating all service definitions under a single services namespace, reducing repetition and making the configuration hierarchy clearer.

--- modules/nixos/hosts/wanda/services.nix
+++ modules/nixos/hosts/wanda/services.nix
@@ -3,72 +3,74 @@
 # Data stored in /var/lib/<service> (NixOS default)
 { ... }:
 {
-  # Plex Media Server
-  # Data: /var/lib/plex
-  services.plex = {
-    enable = true;
-    openFirewall = true;
-    user = "michael";
-    group = "media";
-  };
+  services = {
+    # Plex Media Server
+    # Data: /var/lib/plex
+    plex = {
+      enable = true;
+      openFirewall = true;
+      user = "michael";
+      group = "media";
+    };
 
-  # Tautulli - Plex monitoring
-  # Data: /var/lib/tautulli
-  services.tautulli = {
-    enable = true;
-    openFirewall = true;
-    port = 8181;
-    user = "michael";
-    group = "media";
-  };
+    # Tautulli - Plex monitoring
+    # Data: /var/lib/tautulli
+    tautulli = {
+      enable = true;
+      openFirewall = true;
+      port = 8181;
+      user = "michael";
+      group = "media";
+    };
 
-  # Radarr - Movie management
-  # Data: /var/lib/radarr/.config/Radarr
-  services.radarr = {
-    enable = true;
-    openFirewall = true;
-    user = "michael";
-    group = "media";
-  };
+    # Radarr - Movie management
+    # Data: /var/lib/radarr/.config/Radarr
+    radarr = {
+      enable = true;
+      openFirewall = true;
+      user = "michael";
+      group = "media";
+    };
 
-  # Sonarr - TV show management
-  # Data: /var/lib/sonarr/.config/Sonarr
-  services.sonarr = {
-    enable = true;
-    openFirewall = true;
-    user = "michael";
-    group = "media";
-  };
+    # Sonarr - TV show management
+    # Data: /var/lib/sonarr/.config/Sonarr
+    sonarr = {
+      enable = true;
+      openFirewall = true;
+      user = "michael";
+      group = "media";
+    };
 
-  # Prowlarr - Indexer management
-  # Data: /var/lib/prowlarr/.config/Prowlarr
-  services.prowlarr = {
-    enable = true;
-    openFirewall = true;
-  };
+    # Prowlarr - Indexer management
+    # Data: /var/lib/prowlarr/.config/Prowlarr
+    prowlarr = {
+      enable = true;
+      openFirewall = true;
+    };
 
-  # Bazarr - Subtitle management
-  # Data: /var/lib/bazarr
-  services.bazarr = {
-    enable = true;
-    openFirewall = true;
-    user = "michael";
-    group = "media";
-  };
+    # Bazarr - Subtitle management
+    # Data: /var/lib/bazarr
+    bazarr = {
+      enable = true;
+      openFirewall = true;
+      user = "michael";
+      group = "media";
+    };
 
-  # Overseerr - Request management
-  # Data: /var/lib/overseerr
-  services.overseerr = {
-    enable = true;
-    openFirewall = true;
-  };
+    # Overseerr - Request management
+    # Data: /var/lib/overseerr
+    overseerr = {
+      enable = true;
+      openFirewall = true;
+    };
 
-  # Kavita - Book/comic server
-  # Data: /var/lib/kavita
-  services.kavita = {
-    enable = true;
-    user = "michael";
-    tokenKeyFile = "/var/lib/kavita/token-key";
+    # Kavita - Book/comic server
+    # Data: /var/lib/kavita
+    kavita = {
+      enable = true;
+      user = "michael";
+      tokenKeyFile = "/var/lib/kavita/token-key";
+    };
   };
 
   # Kavita doesn't have openFirewall option, open port manually
