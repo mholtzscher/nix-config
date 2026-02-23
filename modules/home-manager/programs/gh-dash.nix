d41bd50be86fd498de423cbb175387a3c2b95427
Michael Holtzscher <michael@holtzscher.com>
2026-02-23 08:35:35 -0600

    refactor(gh-dash): remove work-specific PR sections from config

--- modules/home-manager/programs/gh-dash.nix
+++ modules/home-manager/programs/gh-dash.nix
@@ -10,21 +10,8 @@
             filters = "is:open author:@me";
           }
           {
-            filters = "repo:paytient/m3p state:open";
-            title = "M3P Open";
-          }
-          {
-            title = "Legends Review Requested";
-            filters = "is:open org:paytient team-review-requested:paytient/legends-of-the-ledger";
-          }
-          {
             title = "Review Requested";
             filters = "is:open review-requested:@me";
-          }
-          {
-            filters = "repo:paytient/m3p";
-            title = "M3P All";
-            limit = 20;
           }
           {
             filters = "is:open owner:mholtzscher";
