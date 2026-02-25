db3fe242709d1f883ee6f4d5e7a7f0c5b8ba1c35
Michael Holtzscher <michael@holtzscher.com>
2026-02-25 08:55:19 -0600

    feat(zellij): add vibes layout with nvim and opencode panes

--- modules/home-manager/programs/zellij.nix
+++ modules/home-manager/programs/zellij.nix
@@ -7,4 +7,5 @@
   # Use the KDL config file directly since home-manager's zellij module
   # doesn't properly escape attribute names with spaces in plugin configs
   xdg.configFile."zellij/config.kdl".source = ../files/zellij.kdl;
+  xdg.configFile."zellij/layouts/vibes.kdl".source = ../files/zellij-layout-vibes.kdl;
 }
