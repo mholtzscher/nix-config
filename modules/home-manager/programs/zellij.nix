82db010280f2a8a8b2f2a972926424e41cfb5636
Michael Holtzscher <michael@holtzscher.com>
2026-03-20 11:14:18 -0500

    refactor(zellij): remove unused vibes layout

--- modules/home-manager/programs/zellij.nix
+++ modules/home-manager/programs/zellij.nix
@@ -7,5 +7,4 @@
   # Use the KDL config file directly since home-manager's zellij module
   # doesn't properly escape attribute names with spaces in plugin configs
   xdg.configFile."zellij/config.kdl".source = ../files/zellij.kdl;
-  xdg.configFile."zellij/layouts/vibes.kdl".source = ../files/zellij-layout-vibes.kdl;
 }
