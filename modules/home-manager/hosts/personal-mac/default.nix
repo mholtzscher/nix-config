fa348fb996e6885b1e48f50980ef768713656ed3
Michael Holtzscher <michael@holtzscher.com>
2026-04-02 20:11:31 -0500

    feat(home-manager): set ZELLIJ_SOCKET_DIR on mac hosts

--- modules/home-manager/hosts/personal-mac/default.nix
+++ modules/home-manager/hosts/personal-mac/default.nix
@@ -2,14 +2,18 @@
 {
   # Personal Mac-specific home-manager configuration
   # This file contains programs and settings unique to the personal M1 Max
+  home = {
+    # Personal Mac-specific programs and packages
+    packages = with pkgs; [
+      discord # Personal communication
+      # macOS-only packages
+      aerospace
+      inputs.aerospace-utils.packages.${pkgs.stdenv.hostPlatform.system}.default
+      mkalias
+    ];
 
-  # Personal Mac-specific programs and packages
-  home.packages = with pkgs; [
-    discord # Personal communication
-
-    # macOS-only packages
-    aerospace
-    inputs.aerospace-utils.packages.${pkgs.stdenv.hostPlatform.system}.default
-    mkalias
-  ];
+    sessionVariables = {
+      ZELLIJ_SOCKET_DIR = "/tmp/zellij";
+    };
+  };
 }
--- modules/home-manager/hosts/work-mac/default.nix
+++ modules/home-manager/hosts/work-mac/default.nix
@@ -4,14 +4,20 @@
   # This file contains programs and settings unique to the work Mac
   # Note: isWork-based configuration (atuin sync, opencode MCPs) is handled
   # in the program modules using the isWork flag
+  home = {
 
-  # Work-specific programs and packages
-  home.packages = with pkgs; [
-    aerospace
-    inputs.aerospace-utils.packages.${pkgs.stdenv.hostPlatform.system}.default
-    mkalias
-    mariadb.client
-    oras
-    sops
-  ];
+    # Work-specific programs and packages
+    packages = with pkgs; [
+      aerospace
+      inputs.aerospace-utils.packages.${pkgs.stdenv.hostPlatform.system}.default
+      mkalias
+      mariadb.client
+      oras
+      sops
+    ];
+
+    sessionVariables = {
+      ZELLIJ_SOCKET_DIR = "/tmp/zellij";
+    };
+  };
 }
