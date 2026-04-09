29fe1d32521f1f67b5b814597d3604622959a441
Michael Holtzscher <michael@holtzscher.com>
2026-04-08 23:45:35 -0500

    feat(home-manager): add plannotator tool and opencode plugin

--- modules/home-manager/programs/mise.nix
+++ modules/home-manager/programs/mise.nix
@@ -3,6 +3,11 @@
   programs = {
     mise = {
       enable = true;
+      globalConfig = {
+        tools = {
+          plannotator = "github:backnotprop/plannotator";
+        };
+      };
     };
   };
 }
