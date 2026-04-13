18a6595f2277b56bd05738bd2a51292256f07251
Michael Holtzscher <michael@holtzscher.com>
2026-04-12 21:20:03 -0500

    chore(pi): trim packages and bump plannotator opencode to 0.17.9

--- modules/home-manager/agents/opencode.nix
+++ modules/home-manager/agents/opencode.nix
@@ -36,7 +36,7 @@
       settings = {
         # share = "disabled";
         plugin = [
-          "@plannotator/opencode@0.17.7"
+          "@plannotator/opencode@0.17.9"
           "@slkiser/opencode-quota"
         ];
         username = "mholtzscher";
