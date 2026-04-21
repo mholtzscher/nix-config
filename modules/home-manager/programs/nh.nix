5865aac9c414ca0400a9dcbb1cff706ff9efff8e
Michael Holtzscher <michael@holtzscher.com>
2026-04-20 21:39:12 -0500

    fix(home-manager): export nh flake paths in nushell

--- modules/home-manager/programs/nh.nix
+++ modules/home-manager/programs/nh.nix
@@ -1,8 +1,23 @@
-{ config, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  nhDarwinFlake = "${config.home.homeDirectory}/.config/nix-config";
+  nhOsFlake = "/home/michael/nix-config";
+in
 {
   programs.nh = {
     enable = true;
-    osFlake = "/home/michael/nix-config";
-    darwinFlake = "${config.home.homeDirectory}/.config/nix-config";
+    osFlake = nhOsFlake;
+    darwinFlake = nhDarwinFlake;
   };
+
+  # Nushell doesn't source home.sessionVariables - set NH_* explicitly
+  programs.nushell.extraConfig = lib.mkAfter ''
+    $env.NH_DARWIN_FLAKE = "${nhDarwinFlake}"
+    $env.NH_OS_FLAKE = "${nhOsFlake}"
+  '';
 }
