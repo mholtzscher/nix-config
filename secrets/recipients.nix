9436eedb052faae84e4850361f15b268f01eb9b7
Michael Holtzscher <michael@holtzscher.com>
2026-06-30 20:42:30 -0500

    feat(secrets): add agenix management script and extract recipients

---
+++ secrets/recipients.nix
@@ -0,0 +1,5 @@
+{
+  # Decrypt with ~/.ssh/id_ed25519_agenix_personal-mac on personal-mac.
+  "personal-mac" =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILk68fR1XZ8WKMgq/1iPem1xfSHY3kM4x2lVdLSSQsPf agenix@personal-mac";
+}
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,6 @@
 let
-  # Decrypt with ~/.ssh/id_ed25519_agenix_personal-mac on personal-mac.
-  personalMac = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILk68fR1XZ8WKMgq/1iPem1xfSHY3kM4x2lVdLSSQsPf agenix@personal-mac";
+  recipients = import ./recipients.nix;
 in
 {
-  "dummy-env.age".publicKeys = [ personalMac ];
+  "dummy-env.age".publicKeys = [ recipients."personal-mac" ];
 }
