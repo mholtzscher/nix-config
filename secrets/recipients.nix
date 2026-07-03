e13691aaca4a76dbbc76eb38a117e0f346acf981
Michael Holtzscher <michael@holtzscher.com>
2026-07-02 21:23:43 -0500

    feat(secrets): add nixos-desktop recipient and re-encrypt dummy-env.age

--- secrets/recipients.nix
+++ secrets/recipients.nix
@@ -2,4 +2,6 @@
   # Decrypt with ~/.ssh/id_ed25519_agenix_personal-mac on personal-mac.
   "personal-mac" =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILk68fR1XZ8WKMgq/1iPem1xfSHY3kM4x2lVdLSSQsPf agenix@personal-mac";
+  "nixos-desktop" =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJxiB4jWljSc3GdmjvKrmYm81XZRoKspTnOPe3WHHxg6 agenix@nixos-desktop";
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,5 +2,8 @@ let
   recipients = import ./recipients.nix;
 in
 {
-  "dummy-env.age".publicKeys = [ recipients."personal-mac" ];
+  "dummy-env.age".publicKeys = [
+    recipients."nixos-desktop"
+    recipients."personal-mac"
+  ];
 }
