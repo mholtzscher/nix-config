05c3b4ed5c52173a2b2911249cf8dcc17bca8071
Michael Holtzscher <michael@holtzscher.com>
2026-07-10 09:35:58 -0500

    fix(ssh): conditionally forward SSH agent based on 1Password socket presence

--- modules/home-manager/programs/ssh.nix
+++ modules/home-manager/programs/ssh.nix
@@ -40,7 +40,11 @@
         };
 
         forwarded-agent = lib.hm.dag.entryBefore [ "*" ] {
-          header = ''Match exec "test -n \"$SSH_CONNECTION\" -a -n \"$SSH_AUTH_SOCK\""'';
+          header =
+            if isDarwin then
+              ''Match exec "test ! -S \"%d/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\""''
+            else
+              ''Match exec "test ! -S \"%d/.1password/agent.sock\""'';
           IdentityAgent = "SSH_AUTH_SOCK";
         };
       };
