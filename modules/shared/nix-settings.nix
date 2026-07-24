6e05ab697a73c7a121e2116675f67b664209c76c
Michael Holtzscher <michael@holtzscher.com>
2026-07-23 19:33:52 -0500

    chore: remove devenv.cachix.org from nix substituters and trusted keys

--- modules/shared/nix-settings.nix
+++ modules/shared/nix-settings.nix
@@ -37,13 +37,11 @@
         "https://mholtzscher.cachix.org"
         "https://nix-community.cachix.org"
         "https://cache.numtide.com"
-        "https://devenv.cachix.org"
       ];
       extra-trusted-public-keys = [
         "mholtzscher.cachix.org-1:liIrpcru/aB3IiCNR62EYTSRPHo/UYYMzYVpYZuiz6w="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
-        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       ];
     };
 
