b4a5cf33adf96b44e60498288a54cac84ca41455
Michael Holtzscher <michael@holtzscher.com>
2026-08-01 14:40:47 -0500

    refactor(ssh): scope 1Password IdentityAgent to macOS only

--- modules/home-manager/programs/ssh.nix
+++ modules/home-manager/programs/ssh.nix
@@ -9,36 +9,32 @@
     ssh = {
       enable = true;
       enableDefaultConfig = false;
-      settings = lib.optionalAttrs (!isWork) {
-        mina-nas = {
-          HostName = "10.69.69.156";
-          User = "root";
-        };
+      settings =
+        lib.optionalAttrs (!isWork) {
+          mina-nas = {
+            HostName = "10.69.69.156";
+            User = "root";
+          };
 
-        max-nas = {
-          HostName = "10.69.69.186";
-          User = "root";
-        };
+          max-nas = {
+            HostName = "10.69.69.186";
+            User = "root";
+          };
 
-        wanda = {
-          HostName = "10.69.69.60";
-          User = "michael";
-        };
+          wanda = {
+            HostName = "10.69.69.60";
+            User = "michael";
+          };
 
-        nixos-desktop = {
-          HostName = "10.69.69.183";
-          User = "michael";
-          ForwardAgent = true;
+          nixos-desktop = {
+            HostName = "10.69.69.183";
+            User = "michael";
+            ForwardAgent = true;
+          };
+        }
+        // lib.optionalAttrs (!isWork && isDarwin) {
+          "*".IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
         };
-
-        "*" = {
-          IdentityAgent =
-            if isDarwin then
-              "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\""
-            else
-              "~/.1password/agent.sock";
-        };
-      };
     };
   };
 }
