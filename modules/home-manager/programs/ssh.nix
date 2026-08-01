f244c033d72d515d59cc6c60c1166dc21c74a2f5
Michael Holtzscher <michael@holtzscher.com>
2026-07-31 19:02:28 -0500

    chore(ssh): remove forwarded-agent config

--- modules/home-manager/programs/ssh.nix
+++ modules/home-manager/programs/ssh.nix
@@ -38,15 +38,6 @@
             else
               "~/.1password/agent.sock";
         };
-
-        forwarded-agent = lib.hm.dag.entryBefore [ "*" ] {
-          header =
-            if isDarwin then
-              ''Match exec "test ! -S \"%d/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\""''
-            else
-              ''Match exec "test ! -S \"%d/.1password/agent.sock\""'';
-          IdentityAgent = "SSH_AUTH_SOCK";
-        };
       };
     };
   };
