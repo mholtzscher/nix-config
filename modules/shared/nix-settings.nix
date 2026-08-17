5a0945306d846561499f6dc2b5a39ee011375c44
Michael Holtzscher <michael@holtzscher.com>
2026-08-17 08:23:46 -0500

    chore(nix-settings): add noctalia.cachix.org substituter and trusted key

--- modules/shared/nix-settings.nix
+++ modules/shared/nix-settings.nix
@@ -36,11 +36,13 @@
       extra-substituters = [
         "https://mholtzscher.cachix.org"
         "https://nix-community.cachix.org"
+        "https://noctalia.cachix.org"
         "https://cache.numtide.com"
       ];
       extra-trusted-public-keys = [
         "mholtzscher.cachix.org-1:liIrpcru/aB3IiCNR62EYTSRPHo/UYYMzYVpYZuiz6w="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
         "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       ];
     };
