8a32cc9c32fa006c1e5be0b9f8f181dc8c035d03
Michael Holtzscher <michael@holtzscher.com>
2026-09-05 20:54:43 -0500

    feat(ssh): use dedicated key for GitHub and scope 1Password agent to home hosts
    Add a github.com entry that uses ~/.ssh/id_ed25519 directly, bypassing
    the 1Password agent for GitHub connections. Restrict the Darwin
    1Password IdentityAgent to the known home hosts (mina-nas, max-nas,
    wanda, nixos-desktop) instead of all hosts, and update the Linux
    forwarded-agent Match rule to the same host set.

--- modules/home-manager/programs/ssh.nix
+++ modules/home-manager/programs/ssh.nix
@@ -4,42 +4,54 @@
   isDarwin,
   ...
 }:
+let
+  onePasswordAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
+in
 {
   programs = {
     ssh = {
       enable = true;
       enableDefaultConfig = false;
-      settings =
-        lib.optionalAttrs (!isWork) {
-          mina-nas = {
-            HostName = "10.69.69.156";
-            User = "root";
-          };
-
-          max-nas = {
-            HostName = "10.69.69.186";
-            User = "root";
-          };
-
-          wanda = {
-            HostName = "10.69.69.60";
-            User = "michael";
-          };
-
-          nixos-desktop = {
-            HostName = "10.69.69.183";
-            User = "michael";
-            ForwardAgent = true;
-          };
-        }
-        // lib.optionalAttrs (!isWork && isDarwin) {
-          "*".IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
-        }
-        // lib.optionalAttrs (!isWork && !isDarwin) {
-          # Preserve a forwarded agent when connected from another machine.
-          "Match host * exec \"sh -c 'test -z \\\"$SSH_CONNECTION\\\"'\"".IdentityAgent =
-            "~/.1password/agent.sock";
+      settings = {
+        "github.com" = {
+          IdentityFile = "~/.ssh/id_ed25519";
+          IdentitiesOnly = true;
+          IdentityAgent = "none";
         };
+      }
+      // lib.optionalAttrs (!isWork) {
+        mina-nas = {
+          HostName = "10.69.69.156";
+          User = "root";
+        };
+
+        max-nas = {
+          HostName = "10.69.69.186";
+          User = "root";
+        };
+
+        wanda = {
+          HostName = "10.69.69.60";
+          User = "michael";
+        };
+
+        nixos-desktop = {
+          HostName = "10.69.69.183";
+          User = "michael";
+          ForwardAgent = true;
+        };
+      }
+      // lib.optionalAttrs (!isWork && isDarwin) {
+        mina-nas.IdentityAgent = onePasswordAgent;
+        max-nas.IdentityAgent = onePasswordAgent;
+        wanda.IdentityAgent = onePasswordAgent;
+        nixos-desktop.IdentityAgent = onePasswordAgent;
+      }
+      // lib.optionalAttrs (!isWork && !isDarwin) {
+        # Preserve a forwarded agent when connected from another machine.
+        "Match host mina-nas,max-nas,wanda,nixos-desktop exec \"sh -c 'test -z \\\"$SSH_CONNECTION\\\"'\"".IdentityAgent =
+          "~/.1password/agent.sock";
+      };
     };
   };
 }
