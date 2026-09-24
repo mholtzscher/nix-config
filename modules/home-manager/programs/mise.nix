896348dbbe0e4dfb68bfcae1c58c8dae1b9f7d75
Michael Holtzscher <michael@holtzscher.com>
2026-09-24 14:37:30 -0500

    Preserve Nushell PATH across mise initialization

--- modules/home-manager/programs/mise.nix
+++ modules/home-manager/programs/mise.nix
@@ -1,6 +1,18 @@
-{ ... }:
+{ lib, ... }:
 {
   programs = {
     mise.enable = true;
+    nushell.extraConfig = lib.mkMerge [
+      (lib.mkOrder 500 ''
+        # Home Manager generates mise's Nushell integration in a Nix build
+        # sandbox, where PATH does not contain the user's profile.
+        $env.__MISE_SHELL_PATH = ($env.PATH | str join (char esep))
+      '')
+      (lib.mkOrder 1500 ''
+        $env.__MISE_ORIG_PATH = $env.__MISE_SHELL_PATH
+        $env.PATH = ($env.__MISE_SHELL_PATH | split row (char esep) | prepend $"($env.HOME)/.local/share/mise/shims")
+        hide-env __MISE_SHELL_PATH
+      '')
+    ];
   };
 }
